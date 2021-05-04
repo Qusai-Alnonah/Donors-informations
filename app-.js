d98ebb04d082c21526b7c2579623e6011f9bc6d3
Qusai-Alnonah <kusaialnona@yahoo.com>
2021-05-04 13:45:14 +0300

    exam

---
+++ app-.js
@@ -0,0 +1,14 @@
+'use stricr';
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+function Donation(name,age,amount){
+  this.name =name;
+  this.amount =amount;
+  this.age= 0;
+}
+
+let firstDonor = new.Donation('Ahmad','20','100');
---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict';
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
+getRandomIntInclusive();
+
+function Donation(name,age,amount){
+  this.name =name;
+  this.amount =amount;
+  this.age= 0;
+  this.push;
+
+}
+
+function genretorAge(){
+  let age = Math.floor(Math.random(18,30));
+}
+genretorAge();
+
+
+function renderTable(){
+  let donortable = document.createElement('table');
+  let tr =document.createElement('tr');
+
+ 
+  
+}
+
